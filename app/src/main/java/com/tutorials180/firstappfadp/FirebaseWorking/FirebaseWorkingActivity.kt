ddf414e0adc143c74dba960ede95949f94bd9a77
codingwithsaani <codingwithsaani@gmail.com>
2022-01-25 18:13:51 +0500

    Version 25 01 22

--- app/src/main/java/com/tutorials180/firstappfadp/FirebaseWorking/FirebaseWorkingActivity.kt
+++ app/src/main/java/com/tutorials180/firstappfadp/FirebaseWorking/FirebaseWorkingActivity.kt
@@ -16,6 +16,11 @@ class FirebaseWorkingActivity : AppCompatActivity()
     private lateinit var mProgressDialog: ProgressDialog
 
     private lateinit var addDocumentBtn:Button
+    private lateinit var addDocumentWithIDBtn:Button
+
+    private lateinit var getSingleDocWithIDBtn:Button
+    private lateinit var deleteSingleDocBtn:Button
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -27,7 +32,16 @@ class FirebaseWorkingActivity : AppCompatActivity()
         mProgressDialog.setTitle("Please wait")
         addDocumentBtn=findViewById(R.id.fb_add_document_btn)
 
+        addDocumentWithIDBtn = findViewById(R.id.fb_add_document_with_id_btn)
+        getSingleDocWithIDBtn = findViewById(R.id.fb_get_doc_with_id_btn)
+
+        deleteSingleDocBtn = findViewById(R.id.fb_delete_doc_with_id_btn)
+
         addDocumentBtn.setOnClickListener { addSingleDocument() }
+        addDocumentWithIDBtn.setOnClickListener { addSingleDocumentWithId() }
+
+        getSingleDocWithIDBtn.setOnClickListener { fetchSingleDocument() }
+        deleteSingleDocBtn.setOnClickListener { deleteSingleDocument() }
     }
 
 
@@ -37,10 +51,10 @@ class FirebaseWorkingActivity : AppCompatActivity()
         {
             mProgressDialog.show()
             var studentData=HashMap<String,Any>()
-            studentData["name"]="Ali Raza"
+            studentData["name"]="Irfan Khan"
 
-            studentData["rollNo"]=1
-            studentData["age"]=20
+            studentData["rollNo"]=10
+            studentData["age"]=40
 
             mFirebaseFirestore.collection("Student")
                 .add(studentData)
@@ -60,10 +74,80 @@ class FirebaseWorkingActivity : AppCompatActivity()
         }
     }
 
+    private fun addSingleDocumentWithId()
+    {
+        try
+        {
+            mProgressDialog.show()
+            var sikandarDoc = HashMap<String,Any>()
+            sikandarDoc["name"] = "Sikandar Khan"
 
+            sikandarDoc["age"] = 22
+            sikandarDoc["rollNo"] = 100
 
+            mFirebaseFirestore.collection("Student").document("Sikandar").set(sikandarDoc)
+                .addOnSuccessListener {_:Void? ->
+                    mProgressDialog.dismiss()
+                    Toast.makeText(applicationContext, "Data is added", Toast.LENGTH_SHORT).show();
+                }
+                .addOnFailureListener {ex ->
+                    mProgressDialog.dismiss()
+                    Toast.makeText(applicationContext, "Data is not added:+ ${ex.message}", Toast.LENGTH_SHORT).show();
+                }
+        }
+        catch (ex:Exception)
+        {
+            mProgressDialog.dismiss()
+            Toast.makeText(applicationContext, ex.message, Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    private fun fetchSingleDocument()
+    {
+        try
+        {
+            mProgressDialog.show()
+            mFirebaseFirestore.collection("Student").document("DqAUQug4IYPBDJihafph")
+                .get()
+                .addOnSuccessListener { doc ->
+                    mProgressDialog.dismiss()
+                    Toast.makeText(applicationContext, "Name : ${doc["name"]}," +
+                            "Rollno : ${doc["rollNo"]}, Age : ${doc["age"]}", Toast.LENGTH_SHORT).show();
+                }
+                .addOnFailureListener { ex ->
+                    mProgressDialog.dismiss()
+                    Toast.makeText(applicationContext, "${ex.message}", Toast.LENGTH_SHORT).show();
+                }
+        }
+        catch (ex:Exception)
+        {
+            mProgressDialog.dismiss()
+            Toast.makeText(applicationContext, "Exception while fetching data: ${ex.message}", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    private fun deleteSingleDocument()
+    {
+        try
+        {
+            mProgressDialog.show()
+            mFirebaseFirestore.collection("Student").document("awais")
+                .delete()
+                .addOnSuccessListener { _:Void? ->
+                    mProgressDialog.dismiss()
+                    Toast.makeText(applicationContext, "Document is deleted", Toast.LENGTH_SHORT).show();
+                }
+                .addOnFailureListener { ex ->
+                    mProgressDialog.dismiss()
+                    Toast.makeText(applicationContext, "Error while deleting : ${ex.message}", Toast.LENGTH_SHORT).show();
+                }
+        }
+        catch (ex:Exception)
+        {
+            mProgressDialog.dismiss()
+            Toast.makeText(applicationContext, "${ex.message}", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 
 
